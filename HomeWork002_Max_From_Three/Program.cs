10240c9d90395c439d4ed78757ebc82d801e13cc
Teymur <timuramiri@yandex.ru>
2022-08-04 23:47:53 +0300

    HomeWork002_Max_From_Three Project

---
+++ HomeWork002_Max_From_Three/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Условия задачи: ");
+Console.WriteLine("Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.");
+
+Console.Write("Задайте первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Задайте второе число: ");
+int b = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Задайте третье число: ");
+int c = Convert.ToInt32(Console.ReadLine()); 
+int max = a;
+
+if(b > a && b > c)
+{
+    max = b;
+}
+if(c > b && c > a)
+{ 
+    max = c; 
+}
+Console.WriteLine("Максимумом является число " + max);
+
