69b0d04c08c1cc3329f55a487ed9dc01fa0db2eb
Teymur <timuramiri@yandex.ru>
2022-08-15 17:54:49 +0300

    Домашнее задание №3

---
+++ HomeWork007_Paliadrom/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+int Number = NameNumber("Введите число: ");
+int NewNumber = 0;
+
+if (Number == Pal(Number, NewNumber))
+{
+    Console.Write("Число является палиндромом");
+}
+else 
+{
+    Console.Write("Число не является палиндромом");
+}
+
+int NameNumber(string message)
+{
+    Console.Write(message);
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+
+int Pal(int a, int c)
+{
+    c = 0;
+    while(a > 0)
+    {
+        int d = a % 10;
+        c = c * 10 + d;
+        a = a / 10;    
+    }
+    return c;
+}
