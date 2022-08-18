36d50879982ef783cb9bd731aa1265e2d250bfa4
TeymurAmiri <109301164+TeymurAmiri@users.noreply.github.com>
2022-08-18 12:59:07 +0300

    Домашняя работа №4

---
+++ HomeWork011_AvB/Program.cs
@@ -0,0 +1,10 @@
+﻿int NameNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = NameNumber("Введите первое число: ");
+int b = NameNumber("Введите второе число: ");
+
+Console.Write("Ответ: " + a + " в степени " + b + " = " + Math.Pow(a, b));
---
+++ HomeWork012_Numbers_Plus/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+int NameNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum(int a)
+{
+    int sum = 0;
+    while(a > 0 || a < 0)
+    {
+        sum += a % 10;
+        a /= 10;
+    }
+    return sum;
+}
+
+
+int number = NameNumber("Введите число: ");
+int a = Sum(number);
+Console.Write("Сумма всех цифр числа " + number + " = " + a);
