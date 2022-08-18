02af748dd12d182e10f2c7ee3bb3245cc3728a2a
Teymur <timuramiri@yandex.ru>
2022-08-18 12:47:40 +0300

    Домашнее задание №4

---
+++ HomeWork013_Massiv_N/Program.cs
@@ -0,0 +1,21 @@
+﻿int NameNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void RandomArray(int a)
+{
+    Console.Write($"Массив из {a} элементов -> ");
+    int[] array = new int[a];
+    for(int i = 0; i < a; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+        Console.Write(array[i] + " ");
+    }
+}
+
+int a = NameNumber("Введите количество элементов массива: ");
+RandomArray(a);
+ 
+
