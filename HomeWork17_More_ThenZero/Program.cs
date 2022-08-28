ebdaeb1d8ec0d7fbd149d32d7786c004c644d7c9
Teymur <timuramiri@yandex.ru>
2022-08-28 18:31:47 +0300

    Homework 6

---
+++ HomeWork17_More_ThenZero/Program.cs
@@ -0,0 +1,71 @@
+﻿string input = NameInt("Введите числа через запятую -> ");
+int[] numbers = ParseStringToArray(input);
+PlusNumbers(numbers);
+
+void PlusNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (numbers[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.Write($"Количество чисел больше нуля в массиве -> {count}");
+}
+
+
+int[] ParseStringToArray(string input)
+{
+    int countNumbers = GetCountNumbersInString(input);
+    int[] numbers = new int[countNumbers];
+    int indexNumber = 0;
+    string substring = string.Empty;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] != ',')
+        {
+            substring += input[i];
+        }
+        else
+        {
+            numbers[indexNumber] = Convert.ToInt32(substring);
+            indexNumber++;
+            substring = string.Empty;
+        }
+    }
+    if (input[input.Length - 1] != ',')
+        numbers[indexNumber] = Convert.ToInt32(substring);
+    return numbers;
+}
+
+int GetCountNumbersInString(string input)
+{
+    int count;
+    if (input[input.Length - 1] == ',')
+        count = 0;
+    else
+        count = 1;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+            count++;
+    }
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+string NameInt(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine();
+}
---
+++ HomeWork18_CrossDot/Program.cs
@@ -0,0 +1,21 @@
+﻿double k1 = NameNumber("Число k1 = ");
+double k2 = NameNumber("Число k2 = ");
+double b1 = NameNumber("Число b1 = ");
+double b2 = NameNumber("Число b2 = ");
+
+CrossCord(k1, k2, b1, b2);
+
+void CrossCord(double a, double b, double c, double d)
+{
+    double x = (d - c)/(a - b);
+    double y1 = a * x + c;
+    double y2 = b * x + d;
+
+    Console.Write($"Точка пересечения: {y1} , {y2}");
+}
+
+double NameNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToDouble(Console.ReadLine());
+}
